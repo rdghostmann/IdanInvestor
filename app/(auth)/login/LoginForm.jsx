9b0cf8795ad17469153817eda6fac5db87987b99
unknown <randalchukzwilson@gmail.com>
2025-02-14 19:53:48 +0100

    Fixing Admin Stuff 4I

--- app/(auth)/login/LoginForm.jsx
+++ app/(auth)/login/LoginForm.jsx
@@ -7,7 +7,7 @@ import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Loader  from 'lucide-react
+import { Loader}  from 'lucide-react'
 
 const LoadingScreen = ({ isLoading }) => {
   if (!isLoading) return null;
